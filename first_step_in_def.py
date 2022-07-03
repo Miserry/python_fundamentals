7ab9c722351aee1bfdbd48c1abf00c03ce16f0a7
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-07-03 19:39:22 +0300

    Add files via upload

---
+++ first_step_in_def.py
@@ -0,0 +1,20 @@
+def add_two(x):   #(x) is called parameter and it works like an input. The user has to write something on its place.
+    return x + 2
+print(add_two(5))
+
+def subtract_two(number):
+    return number - 2
+print (subtract_two(34232))
+
+def print_string(string):
+    print(string)
+print_string('Hello!')
+
+def acceleration(mass, force):
+    a = mass * force
+    return a
+print(acceleration(4, 6))
+
+def do_something():
+    print('hi')
+do_something()

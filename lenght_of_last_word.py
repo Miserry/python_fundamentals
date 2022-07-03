f30f0e76cb99bc917129c13db8ab5f836e1c1ffc
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-07-04 01:27:27 +0300

    Add files via upload

---
+++ lenght_of_last_word.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        list_s = s.split()
+        fin_list = [s.strip() for s in list_s] 
+        lenght = len(list_s[-1])
+        return lenght
+
+
+
+

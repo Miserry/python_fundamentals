5269fce1de58fd20465d3177aa1a5a559ac3ad98
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-07-11 15:43:03 +0300

    Update calculator_test.py

--- calculator_test.py
+++ calculator_test.py
@@ -1,3 +1,6 @@
+#The whole purpose of this code is to test the calculator.py and tell if we messed up something with the function there.
+
+
 from calculator import square
 
 
@@ -14,3 +17,7 @@ def test_square():
 
 if __name__ == "__main__":
     main()
+
+    
+#We can even run this with pytest, which i did and it works just fine.
+#Doesn't look particularly necessary at this point, but still good to know.

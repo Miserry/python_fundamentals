ca10f61baa09daa83c0f562a79ec76aa44691dc9
Hristo Lakov <107894257+Miserry@users.noreply.github.com>
2022-07-11 15:40:53 +0300

    Add files via upload

---
+++ calculator_test.py
@@ -0,0 +1,16 @@
+from calculator import square
+
+
+def main():
+    test_square()
+
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-4) == 16
+
+
+if __name__ == "__main__":
+    main()
